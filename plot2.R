04e64d56b8cb9ff2c64afc08b3bca008964735f2
Lawrence Chen <lawrence.chen@experian.com>
2015-04-10 16:35:30 -0700

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,10 @@
+#source("load.R")
+
+plot(household_power_consumption$Time,
+     household_power_consumption$Global_active_power,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot2.png", width = 480, height = 480) #default is 480px x 480px
+dev.off()
