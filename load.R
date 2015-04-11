561d0d46260f7a9b5044bf83d9ae5fe8cfe827ed
Lawrence Chen <lawrence009@outlook.com>
2015-04-11 08:50:45 -0700

    close sqldf conn before transforming data

--- load.R
+++ load.R
@@ -4,10 +4,10 @@ filename <- "./household_power_consumption.txt"
 household_power_consumption <- read.csv.sql(filename,
                                             "select * from file where Date = '1/2/2007' or Date = '2/2/2007'",
                                             sep=";")
+# close connection
+sqldf()
+
 
 household_power_consumption$Time <- strptime(paste(household_power_consumption$Date, household_power_consumption$Time),
                                              "%d/%m/%Y %T")
 household_power_consumption$Date <- as.Date(household_power_consumption$Time)
-
-# close connection
-sqldf()
