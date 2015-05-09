8cba5e3e4e02a0f873dc39befa4c30662d9fd9c6
Lawrence Chen <lawrence009@outlook.com>
2015-05-09 12:14:31 -0700

    amend comments to load script

--- load.R
+++ load.R
@@ -1,11 +1,22 @@
-library(sqldf)
+#Read data into R filtering it with an sql statement. Only the filtered
+#portion is processed by R so that files larger than R can otherwise handle
+#can be accommodated.
+
+
 filename <- "./household_power_consumption.txt"
+
+#the common way; read all then subset 
 # household_power_consumption <- read.csv(filename, sep=";", quote="", na.strings="?")
+# household_power_consumption <- household_power_consumption[household_power_consumption$Date == "1/2/2007" |
+#                                                            household_power_consumption$Date == "2/2/2007"
+#                                                            , ]
+
+#or use sqldf; still read the entire file but only the selected, i.e., filtered
+#portion is processed by R
+library(sqldf)
 household_power_consumption <- read.csv.sql(filename,
                                             "select * from file where Date = '1/2/2007' or Date = '2/2/2007'",
                                             sep=";")
-# close connection
-sqldf()
 
 
 household_power_consumption$Time <- strptime(paste(household_power_consumption$Date, household_power_consumption$Time),
